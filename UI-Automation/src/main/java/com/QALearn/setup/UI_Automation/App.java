c1c6f7e4689e0510cbd3edc4c27d00931d666efd
vinay <vinay@LAPTOP-JKHN369G>
2024-08-09 10:52:07 +0530

    Initial Commit

---
+++ UI-Automation/src/main/java/com/QALearn/setup/UI_Automation/App.java
@@ -0,0 +1,13 @@
+package com.QALearn.setup.UI_Automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
