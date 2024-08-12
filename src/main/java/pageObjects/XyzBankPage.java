e67444b43bc8e84aded821b179d112106121c696
vinay <vinay@LAPTOP-6T67O2R4>
2024-08-12 20:31:17 +0530

    added locators for customer login module

--- src/main/java/pageObjects/XyzBankPage.java
+++ src/main/java/pageObjects/XyzBankPage.java
@@ -12,8 +12,6 @@ public class XyzBankPage extends Testbase {
 	WebElement header;
 	@FindBy(xpath = "//button[contains(.,'Home')]")
 	WebElement homeBtn;
-	@FindBy(xpath = "//button[contains(text(),'Customer Login')]")
-	WebElement customerLoginBtn;
 	@FindBy(xpath = "//button[contains(text(),'Bank Manager Login')]")
 	WebElement bankerLoginBtn;
 	@FindBy(xpath = "//button[contains(.,'Add Customer') and @ng-class='btnClass1']")
@@ -38,6 +36,37 @@ public class XyzBankPage extends Testbase {
 	WebElement customerTab;
 	@FindBy(xpath = "//td[contains(.,'%s')]//following::td[position()=4]")  
 	WebElement customerDelBtn; //customer name to be parameterized
+	@FindBy(xpath = "//button[contains(text(),'Customer Login')]")
+	WebElement customerLoginBtn;
+	@FindBy(xpath = "#userSelect")
+	WebElement customerDrp; //customer name to be selected from dropdown
+	@FindBy(xpath = "//button[contains(text(),'Login')]")
+	WebElement loginBtn;
+	@FindBy(xpath = "//button[@class='btn logout']")
+	WebElement logoutBtn;
+	@FindBy(xpath = "#accountSelect")
+	WebElement accountDrp;
+	@FindBy(xpath = "//button[contains(text(),'Deposit') and @ng-class='btnClass2']")
+	WebElement depositTab;
+	@FindBy(xpath = "//input[@placeholder='amount']")
+	WebElement amountInput; //common for deposit & withdraw
+	@FindBy(xpath = "//button[contains(.,'Deposit') and @type='submit']")
+	WebElement depositBtn;
+	@FindBy(xpath = "//button[contains(text(),'Withdrawl')]")
+	WebElement withdrawTab;
+	@FindBy(xpath = "//button[contains(text(),'Withdraw') and @type='submit' ]")
+	WebElement withdrawBtn;
+	@FindBy(xpath = "//button[contains(text(),'Transactions')]")
+	WebElement tansactionsBtn;
+	@FindBy(xpath = "//button[contains(text(),'Back')]")
+	WebElement backBtn;
+	@FindBy(xpath = "//button[contains(text(),'Reset')]")
+	WebElement resetBtn;
+	@FindBy(xpath = "#start")
+	WebElement startDate; //format dd-mm-yyyy --:--
+	@FindBy(xpath = "#end")
+	WebElement endDate; //format dd-mm-yyyy --:--
+	
 	
 		
 	public XyzBankPage() {
